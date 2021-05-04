3a6571bda42ac7a6f183a906817c37e89ed158d4
Massimo <massimo.rosas279@gmail.com>
2021-05-04 15:07:55 +0200

    footer basic css

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,12 @@ import './footer.scss';
 
 const Footer = () => (
   <footer className="footer">
-    <h2 className="footer-title">Contact</h2>
+    <h2 className="footer-title">Liens</h2>
+    <div className="footer-links">
+      <a href="https://www.linkedin.com/in/massimo-rosas/" target="_blank" rel="noreferrer"><i className="devicon-linkedin-plain footer-logo" /> </a>
+      <a href="https://github.com/MassimoRosas" target="_blank" rel="noreferrer"><i className="devicon-github-original footer-logo" /> </a>
+      <a href="mailto:massimo.rosas@outlook.fr"><i className="fas fa-at footer-logo" /> </a>
+    </div>
   </footer>
 );
 
