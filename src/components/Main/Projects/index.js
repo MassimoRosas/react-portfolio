37642d9b62cc7a19f5ddf0b0dc06722b1edc1145
Massimo <massimo.rosas279@gmail.com>
2021-05-05 15:35:16 +0200

    projects style v2

--- src/components/Main/Projects/index.js
+++ src/components/Main/Projects/index.js
@@ -10,7 +10,7 @@ import reactTodoList from '../../../assets/images/reactTodoList.png';
 const Projects = () => (
   <section className="main-section main-section-projects" id="projects">
     <h2 className="projects-title">Projets</h2>
-    <div className="projects-highlight">
+    <div className="projects-highlight card">
       <img className="projects-highlight-preview" src={FeelBetter} alt="Aperçu Feel Better" />
       <div className="projects-highlight-description">
         <h3 className="projects-highlight-description-title">Feel Better</h3>
@@ -26,25 +26,29 @@ const Projects = () => (
           <li className="projects-highlight-description-list-item">mocha et chai pour les tests unitaires</li>
         </ul>
       </div>
-      <iframe src="https://www.youtube.com/embed/sSp3eACAeU0?start=2365" title="Présentation YouTube Feel Better" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+      <iframe className="projects-highlight-video" src="https://www.youtube.com/embed/sSp3eACAeU0?start=2365" title="Présentation YouTube Feel Better" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
     </div>
     <div className="projects-list-container">
       <ul className="projects-list">
-        <li className="projects-list-item">
+        <li className="projects-list-item card">
           <img className="projects-list-item-preview" src={reactConverter} alt="react-converter preview" />
           <h3 className="projects-list-item-title">React Converter</h3>
+          <a className="projects-list-item-link" href="https://react-converter.vercel.app/" target="_blank" rel="noreferrer">Lien</a>
         </li>
-        <li className="projects-list-item">
+        <li className="projects-list-item card">
           <img className="projects-list-item-preview" src={reactDashboard} alt="react-dashboard preview" />
           <h3 className="projects-list-item-title">React Social Media Dashboard</h3>
+          <a className="projects-list-item-link" href="https://react-converter.vercel.app/" target="_blank" rel="noreferrer">Lien</a>
         </li>
-        <li className="projects-list-item">
+        <li className="projects-list-item card">
           <img className="projects-list-item-preview" src={reactGitHubApi} alt="react-github-api preview" />
           <h3 className="projects-list-item-title">React GitHub API Search</h3>
+          <a className="projects-list-item-link" href="https://react-converter.vercel.app/" target="_blank" rel="noreferrer">Lien</a>
         </li>
-        <li className="projects-list-item">
+        <li className="projects-list-item card">
           <img className="projects-list-item-preview" src={reactTodoList} alt="react-to-do-list preview" />
           <h3 className="projects-list-item-title">React To-Do List</h3>
+          <a className="projects-list-item-link" href="https://react-converter.vercel.app/" target="_blank" rel="noreferrer">Lien</a>
         </li>
       </ul>
     </div>
