546db894b06bc08db308cc0c448ac2398372f314
Massimo <massimo.rosas279@gmail.com>
2021-05-06 13:32:39 +0200

    responsive for tablets

--- src/components/Main/Career/index.js
+++ src/components/Main/Career/index.js
@@ -7,27 +7,30 @@ import './career.scss';
 const Career = () => (
   <section className="main-section main-section-career" id="career">
     <h2 className="career-title">Parcours</h2>
-    <div className="career-oclock card">
-      <div className="career-logo">
-        <img className="career-logo-img" src={logoOclock} alt="Logo O'Clock" />
-        <span className="career-logo-title">O'Clock</span>
+    <div className="career-container">
+      <div className="career-oclock card">
+        <div className="career-logo">
+          <img className="career-logo-img" src={logoOclock} alt="Logo O'Clock" />
+          <span className="career-logo-title">O'Clock</span>
+        </div>
+        <div className="career-description">
+          <h3 className="career-description-title">Développement d'applications web / web-mobile (niveau Bac+2)</h3>
+          <h4 className="career-description-subtitle">5 mois intensifs (700 heures) en télé-présentiel</h4>
+          <p className="career-description-single">3 mois de Socle: HTML, CSS, JS, PHP, MySQL</p>
+          <p className="career-description-single">1 mois de spécialisation: React</p>
+          <p className="career-description-single">1 mois de projet en groupe</p>
+        </div>
       </div>
-      <div className="career-description">
-        <h3 className="career-description-title">Développement d'applications web / web-mobile (niveau Bac+2)</h3>
-        <h4 className="career-description-subtitle">5 mois intensifs (700 heures) en télé-présentiel</h4>
-        <p className="career-description-single">3 mois de Socle: HTML, CSS, JS, PHP, MySQL</p>
-        <p className="career-description-single">1 mois de spécialisation: React</p>
-        <p className="career-description-single">1 mois de projet en groupe</p>
+      <div className="career-opquast card">
+        <img className="career-description-badge" src={opquastBadge} alt="Badge Opquast Confirmé" />
+        <span className="career-logo-title">Certification Opquast</span>
+        <h3 className="career-description-title">"Maîtrise de la qualité en projet Web"</h3>
+        <h4 className="career-description-subtitle">Certification de référence pour les métiers du Web</h4>
+        <p className="career-description-single">14 heures de formation en autonomie</p>
+        <p className="career-description-single">240 règles réalistes et vérifiables, validées par la communauté</p>
+        <p className="career-description-single">Concerne le modèle VPTCS (Elie Sloïm et Eric Gateau) : Visibilité, Perception, Technique, Contenus, Services</p>
+        <p className="career-description-result">Résultat obtenu: 860 / 1000 points</p>
       </div>
-    </div>
-    <div className="career-opquast card">
-      <img className="career-description-badge" src={opquastBadge} alt="Badge Opquast Confirmé" />
-      <h3 className="career-description-title">Certification Opquast : "Maîtrise de la qualité en projet Web"</h3>
-      <h4 className="career-description-subtitle">Certification de référence pour les métiers du Web</h4>
-      <p className="career-description-single">14 heures de formation en autonomie</p>
-      <p className="career-description-single">240 règles réalistes et vérifiables, validées par la communauté</p>
-      <p className="career-description-single">Concerne le modèle VPTCS (Elie Sloïm et Eric Gateau) : Visibilité, Perception, Technique, Contenus, Services</p>
-      <p className="career-description-result">Résultat obtenu: 860 / 1000 points</p>
     </div>
   </section>
 );
--- src/components/Main/Home/index.js
+++ src/components/Main/Home/index.js
@@ -10,39 +10,44 @@ const Home = () => (
       <h3 className="home-subtitle">Développeur Frontend</h3>
       <p className="home-paragraph">React</p>
     </div>
-    <img className="home-avatar" src={avatar} alt="Massimo" />
-    <ul className="home-data">
-      <li className="home-data-single">
-        <i className="fas fa-map-marker-alt home-data-single-logo" />
-        <p className="home-data-single-description">35580 Guichen</p>
-      </li>
-      <li className="home-data-single">
-        <i className="fas fa-at home-data-single-logo" />
-        <p className="home-data-single-description">massimo.rosas@outlook.fr</p>
-      </li>
-      <li className="home-data-single">
-        <i className="fas fa-mobile-alt home-data-single-logo" />
-        <p className="home-data-single-description">06.24.12.36.94</p>
-      </li>
-    </ul>
-    <div className="home-presentation card">
-      <p className="home-presentation-single">
-        A la base détenteur d'un BTS Technicien
-        Biochimiste / Microbiologiste, j'ai choisi
-        de me reconvertir dans le développement web,
-        plus particulièrement dans le développement frontend.
-      </p>
-      <p className="home-presentation-single">
-        Avant cela, j'ai travaillé trois ans en grande
-        distribution, d'abord en employé libre-service, puis en responsable-adjoint de secteur frais
-        en Haute-Savoie, où j'ai toujours vécu.
-        J'ai ensuite décidé de m'installer en Ille-et-Vilaine
-        autour de Rennes.
-      </p>
-      <p className="home-presentation-single">
-        J'ai pu acquérir de très bonnes compétences en travail d'équipe,
-        en organisation et en relation client.
-      </p>
+    <div className="home-first-container">
+      <div className="home-second-container">
+        <img className="home-avatar" src={avatar} alt="Massimo" />
+        <ul className="home-data">
+          <li className="home-data-single">
+            <i className="fas fa-map-marker-alt home-data-single-logo" />
+            <p className="home-data-single-description">35580 Guichen</p>
+          </li>
+          <li className="home-data-single">
+            <i className="fas fa-at home-data-single-logo" />
+            <p className="home-data-single-description">massimo.rosas@outlook.fr</p>
+          </li>
+          <li className="home-data-single">
+            <i className="fas fa-mobile-alt home-data-single-logo" />
+            <p className="home-data-single-description">06.24.12.36.94</p>
+          </li>
+        </ul>
+      </div>
+      <div className="home-presentation card">
+        <p className="home-presentation-single">
+          A la base détenteur d'un BTS Technicien
+          Biochimiste / Microbiologiste, j'ai choisi
+          de me reconvertir dans le développement web,
+          plus particulièrement dans le développement frontend.
+        </p>
+        <p className="home-presentation-single">
+          Avant cela, j'ai travaillé trois ans en grande
+          distribution, d'abord en employé libre-service,
+          puis en responsable-adjoint de secteur frais
+          en Haute-Savoie, où j'ai toujours vécu.
+          J'ai ensuite décidé de m'installer en Ille-et-Vilaine
+          autour de Rennes.
+        </p>
+        <p className="home-presentation-single">
+          J'ai pu acquérir de très bonnes compétences en travail d'équipe,
+          en organisation et en relation client.
+        </p>
+      </div>
     </div>
   </section>
 );
