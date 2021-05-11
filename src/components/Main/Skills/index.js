0ec1aa302b7e1a7bdab0166df1b94e048091c71d
Massimo <massimo.rosas279@gmail.com>
2021-05-11 17:12:55 +0200

    added subtitles for skills

--- src/components/Main/Skills/index.js
+++ src/components/Main/Skills/index.js
@@ -8,6 +8,7 @@ import './skills.scss';
 const Skills = () => (
   <section className="main-section main-section-skills" id="skills">
     <h2 className="skills-title">Compétences</h2>
+    <h3 className="skills-subtitle">Savoir-faire</h3>
     <ul className="skills-list">
       {hardSkillsData.map((hardSkill) => (
         <li key={hardSkill.name} className="skills-list-item card"><i className={`${hardSkill.class} skills-list-item-icon`} /><span className="skills-list-item-description">{hardSkill.name}</span></li>
@@ -15,6 +16,7 @@ const Skills = () => (
     </ul>
     <br />
     <br />
+    <h3 className="skills-subtitle">Savoir-être</h3>
     <ul className="skills-list">
       {softSkillsData.map((softSkill) => (
         <li key={softSkill} className="skills-list-item card"><span className="skills-list-item-description">{softSkill}</span></li>
